41fdb4ef7737033850c9ce5090a7e46a04fdbb66
hpetrow <heather.petrow@gmail.com>
2015-07-10 11:28:19 -0400

    done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+
+def prime_number_for(n)
+  prime_count = 0
+  i = 1
+  while prime_count < n
+    prime_count += 1 if is_prime(i)
+    i += 2
+  end
+  i - 2
+end
+
+def is_prime(n)
+  if n < 1 || n%2 == 0
+    false
+  elsif n == 1
+    true
+  else
+    prime = true
+    i = 3
+    while (prime && i <= Math.sqrt(n).floor)
+      prime = false if n % i == 0
+      i += 2
+    end
+    prime
+  end
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,35 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  attr_reader :number
+
+  def initialize(n)
+    @number = prime_number_for(n)
+  end
+
+  def prime_number_for(n)
+    prime_count = 0
+    i = 1
+    while prime_count < n
+      prime_count += 1 if is_prime(i)
+      i += 2
+    end
+    i - 2
+  end
+
+  def is_prime(n)
+    if n < 1 || n%2 == 0
+      false
+    elsif n == 1
+      true
+    else
+      prime = true
+      i = 3
+      while (prime && i <= Math.sqrt(n).floor)
+        prime = false if n % i == 0
+        i += 2
+      end
+      prime
+    end
+  end
+end
